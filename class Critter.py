14e8528e10cd620200b000062a5dea4b14baca5e
GlebusDedus <gleb111204@gmail.com>
2020-09-19 17:51:44 +0300

    da

--- class Critter.py
+++ class Critter.py
@@ -1,21 +1,45 @@
 class Critter:
     total = 0
 
+    @property
+    def mood(self):
+        unhappiness =self.hunger + self.boredom
+        if unhappiness < 5:
+            m = "briliant"
+        elif 5<= unhappiness <=10:
+            m="good"
+        elif 11<= unhappiness <=15:
+            m="not bad"
+        else:
+            m="bad"
+        return m
+
     @staticmethod
     def status():
         print("общее число зверюшек",Critter.total)
 
     def __init__(self,name,hunger=0,boredom=0):
-        self.name = name
+        self.__name = name
         self.hunger = hunger
         self.boredom = boredom
         Critter.total +=1
     def talk(self):
-        print("меня зовут",self.name)
+        print("меня зовут",self.name,
+        ", и сейчас я чувстую себя", self.mood)
     def __str__(self):
         ans = 'Обьект класса Critter\n'
         ans += 'имя: ' + self.name + '\n'
         return ans
+    def eat(self,food = 4):
+        print("Мррр... Спасибо!")
+        self.hunger -= food
+        if self.hunger < 0:
+            self.hunger = 0
+        self.__pass_time()
+
+    def __pass_time(self):
+        self.hunger +=1
+        self.boredom +=1
 
 def main():
     print("доступ к атрибуту класса Critter.total:", end=" ")
@@ -27,7 +51,6 @@ def main():
     crit3 = Critter("зверюшек 3")
     Critter.status()
 
-    print('доступ к атрибуту класса через сбьект:', end='')
-    print(crit1.total)
+    print('доступ к атрибуту класса через сбьект:', crit1.mood)
 
 main()
